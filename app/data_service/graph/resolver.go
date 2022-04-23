eaac032da3d26e5b2b99c672b3f5cb357f2bafa3
jecosine <jecosine@gmail.com>
2022-04-24 04:32:34 +0800

    feat(gql): generate gql resolvers files

--- app/data_service/graph/resolver.go
+++ app/data_service/graph/resolver.go
@@ -1,11 +1,22 @@
+//go:generate go run github.com/99designs/gqlgen generate
 package graph
 
-import "github.com/Jecosine/alioth-kratos/app/data_service/graph/model"
+import "github.com/Jecosine/alioth-kratos/app/data_service/pkg/resolver"
 
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
 type Resolver struct {
-	todos []*model.Todo
+	//todos []*model.Todo
+}
+
+func (r Resolver) Mutation() resolver.MutationResolver {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r Resolver) Query() resolver.QueryResolver {
+	//TODO implement me
+	panic("implement me")
 }
---
+++ app/data_service/pkg/resolver/schema.resolvers.go
@@ -0,0 +1,28 @@
+package resolver
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Jecosine/alioth-kratos/app/data_service/pkg/model"
+)
+
+func (r *mutationResolver) CreateTodo(ctx context.Context, payload *string) (*model.Todo, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// Mutation returns MutationResolver implementation.
+func (r *Resolver) Mutation() MutationResolver { return &mutationResolver{r} }
+
+// Query returns QueryResolver implementation.
+func (r *Resolver) Query() QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
