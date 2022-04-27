67c8d128c1bfb4bbe45d752a6d0dd8cd929aaaf9
jecosine <jecosine@gmail.com>
2022-04-27 20:16:30 +0800

    feat(data_service): implement user resolver

--- app/data_service/internal/resolver/user.resolvers.go
+++ app/data_service/internal/resolver/user.resolvers.go
@@ -79,10 +79,6 @@ func (r *queryResolver) Users(ctx context.Context) ([]*ent.User, error) {
 	return r.client.User.Query().All(ctx)
 }
 
-func (r *userResolver) Managed(ctx context.Context, obj *ent.User) ([]*ent.Team, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
 func (r *userResolver) Announcements(ctx context.Context, obj *ent.User) ([]*ent.Announcement, error) {
 	panic(fmt.Errorf("not implemented"))
 }
@@ -95,3 +91,16 @@ func (r *userResolver) Records(ctx context.Context, obj *ent.User) ([]*ent.Judge
 func (r *Resolver) User() UserResolver { return &userResolver{r} }
 
 type userResolver struct{ *Resolver }
+
+// !!! WARNING !!!
+// The code below was going to be deleted when updating resolvers. It has been copied here so you have
+// one last chance to move it out of harms way if you want. There are two reasons this happens:
+//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
+//    it when you're done.
+//  - You have helper methods in this file. Move them out to keep these resolver files clean.
+func (r *userResolver) Managed(ctx context.Context, obj *ent.User) ([]*ent.Team, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+func (r *userResolver) Owned(ctx context.Context, obj *ent.User) ([]*ent.Team, error) {
+	panic(fmt.Errorf("not implemented"))
+}
